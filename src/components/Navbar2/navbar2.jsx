3fae8c501630cf755c7b5823ff43b3029e43aa44
Hilalfadilah <hilalfadilah21@gmail.com>
2023-09-04 16:59:49 +0700

    add function logout& fix components

--- src/components/Navbar2/navbar2.jsx
+++ src/components/Navbar2/navbar2.jsx
@@ -1,32 +1,135 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../Button/Button";
 
 export default function Navbar2() {
+  const navigate = useNavigate();
+
+  function removeCookie(name) {
+    document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+  }
+
+  const handleLogout = () => {
+    removeCookie("user_token");
+    navigate("/login");
+  };
   return (
-    <div className="fixed z-50 bottom-0 w-full h-[5rem] bg-white border-t-2 border-solid border-cyan-200 flex items-center">
-      <div className="w-10/12 mx-auto flex items-center justify-center gap-[4rem]">
-        <h1 className="text-[32px] font-semibold">HilalStore</h1>
-        <div className="flex items-center w-full justify-between">
-          <div className="flex items-center justify-center gap-[1.5rem]">
-            <Link to={"google.com"}>Produk</Link>
-            <Link to={"google.com"}>Tentang Kami</Link>
-          </div>
-          <div className="flex items-center justify-center gap-[2.75rem]">
-            <input
-              type="search"
-              id="search"
-              class="block w-full p-3 border border-neutral-500 shadow-md rounded-2xl focus:ring-blue-500 focus:border-blue-500 placeholder:text-gray-600"
-              placeholder="Cari Produk"
-              required=""
-            />
-            <img
-              className="cursor-pointer"
-              alt="Span"
-              src="https://anima-uploads.s3.amazonaws.com/projects/64e16eeb6df9651ba7fabf57/releases/64e1701f7ab2fa75b7d880a3/img/span.svg"
-            />
+    <>
+      <div className="fixed top-0 z-10 w-full h-[5rem] bg-white border-b-2 border-solid border-cyan-200 flex items-center">
+        <div className="w-full mx-5 lg:w-10/12 lg:mx-auto lg:flex lg:items-center lg:justify-center lg:gap-[4rem]">
+          <Link
+            to={"/"}
+            className="text-[32px] font-semibold -tracking-tighter bg-gradient-to-r from-purple-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient"
+          >
+            HilalStore
+          </Link>
+          <div className="hidden lg:flex items-center w-full justify-between">
+            <div className="flex items-center justify-center gap-[1.5rem]">
+              <Link to={"/produk"}>Produk</Link>
+              <Link to={"/about"}>Tentang Kami</Link>
+            </div>
+            <div className="flex items-center justify-center gap-[2.75rem]">
+              <input
+                type="search"
+                id="search"
+                className="block w-full p-3 border border-neutral-500 shadow-md rounded-2xl focus:ring-blue-500 focus:border-blue-500 placeholder:text-gray-600"
+                placeholder="Cari Produk"
+                required=""
+              />
+              <img
+                className="cursor-pointer"
+                alt="Span"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64e16eeb6df9651ba7fabf57/releases/64e1701f7ab2fa75b7d880a3/img/span.svg"
+              />
+              <Button
+                text="Log out"
+                className="w-36 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                onClick={handleLogout}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {/* Mobile Menu */}
+      <div className="fixed bottom-0 left-0 z-50 w-full h-16 lg:hidden bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+        <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
+          <Link
+            to={"/"}
+            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <svg
+              className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+            </svg>
+            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+              Home
+            </span>
+          </Link>
+          <Link
+            to={"/produk"}
+            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <svg
+              className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z" />
+              <path d="M19 10h-6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1Zm-4.5 3.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM12.62 4h2.78L12.539.41a1.086 1.086 0 1 0-1.7 1.352L12.62 4Z" />
+            </svg>
+            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+              Produk
+            </span>
+          </Link>
+          <Link
+            to={"/about"}
+            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <svg
+              className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"
+              />
+            </svg>
+            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+              About
+            </span>
+          </Link>
+          <button
+            onClick={handleLogout}
+            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            <svg
+              className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+            </svg>
+            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+              Log out
+            </span>
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
